ea667d72f7737c3ba2aba82d5311fa5bc9f8edcf
sultanko <egorshah@gmail.com>
2013-08-12 12:06:48 +0300

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TravianBot::Application.config.secret_key_base = '6abffe6112b948092b9c656555af591c221d8f7731e8868639b7d1156c96dccc720ec99ccb63489410e86864f935f3b2890d01eab62de72dbcf1505c8b9bb07a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TravianBot::Application.config.session_store :cookie_store, key: '_travian_bot_session'
